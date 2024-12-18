661171a7a3375e7a8246feb6500c52632e551f61
KarimPozzato <94352240+KarimPozzato@users.noreply.github.com>
2024-12-18 15:48:41 +0100

    Sito Socket

---
+++ SitoSocket/script.js
@@ -0,0 +1,20 @@
+const alertPlaceholder = document.getElementById('liveAlertPlaceholder')
+const appendAlert = (message, type) => {
+  const wrapper = document.createElement('div')
+  wrapper.innerHTML = [
+    `<div class="alert alert-${type} alert-dismissible" role="alert">`,
+    `   <div>${message}</div>`,
+    '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
+    '</div>'
+  ].join('')
+
+  alertPlaceholder.append(wrapper)
+}
+
+const alertTrigger = document.getElementById('liveAlertBtn')
+if (alertTrigger) {
+  alertTrigger.addEventListener('click', () => {
+    appendAlert('Prima Pagina introduttiva sulle socket!','success')
+    appendAlert('Bene Ora Può cominciare a scoprire tutto sulle socket','success')
+  })
+}
